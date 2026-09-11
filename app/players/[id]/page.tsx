2f3a84535fa8b5f86db7ab1990d843c26163fae0
reidpsmith3-create <reidpsmith3@gmail.com>
2026-09-11 14:40:05 +0000

    Refresh player profile stats dynamically

--- app/players/[id]/page.tsx
+++ app/players/[id]/page.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import { supabase } from "@/lib/supabase";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 type PlayerPageProps = {
   params: {
     id: string;
